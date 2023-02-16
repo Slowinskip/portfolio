cf0d22cf3d5586139e1ae075af965950ba8338d5
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-02-16 11:05:15 +0100

    Add nav and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Col, Container, Row } from 'react-bootstrap'
+import styles from './app.module.scss'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <nav className={styles.nav}>
+        <Container>
+          <Row>
+            <Col>
+              <div className={styles.navLogo}>
+                <p className={styles.navP}>P</p>
+                <p>atrick</p>
+              </div>
+            </Col>
+            <Col>
+              <div className={styles.navLinks}>
+                <a href="#1">About</a>
+                <a href="#1">Skills</a>
+                <a href="#1">Work</a>
+                <a href="#1">Contact</a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </nav>
+      <div className={styles.bodyContainer}>
+        <Container>
+          <h1 className={styles.h}>1</h1>
+        </Container>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
